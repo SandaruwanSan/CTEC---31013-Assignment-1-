3605d7de17e67ef9af537ce26828451f4b9c613c
SandaruwanSan <sandaruwanvssanrider@gamil.com>
2023-12-04 09:46:29 +0530

    Added Undo and Redo Buttons to the html file and function codes to the js file

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,11 @@
 // CT-2019-030 - JavaScripts
+
+//Functions for Redo and Undo
+
+function execUndo() {
+    document.execCommand('undo', false, null);
+}
+
+function execRedo() {
+    document.execCommand('redo', false, null);
+}
